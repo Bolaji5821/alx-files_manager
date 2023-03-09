fea14e415dc84c3d8d1affa5980dee82c2f1989f
Bolaji581 <oyedelebolaji84@gmail.com>
2023-03-09 04:43:15 +0100

    First file

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import UtilController from '../controllers/UtilController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+
 
 const router = Router();
 
@@ -34,6 +36,7 @@ router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
+router.post('/files', FilesController.postUpload);
 router.get('/disconnect', AuthController.getDisconnect);
 
 module.exports = router;
